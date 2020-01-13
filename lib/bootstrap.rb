578d7fd5302c1fa17dafdca1f3df2e00ada16390
Cyberarm <matthewlikesrobots@gmail.com>
2020-01-13 17:01:23 -0600

    Made cache unhidden

--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -12,4 +12,4 @@ end
 CyberarmLauncher::ROOT_PATH = File.expand_path("../../", __FILE__)
 CyberarmLauncher::ASSETS_PATH = File.expand_path("../../assets", __FILE__)
 CyberarmLauncher::APPLICATIONS_PATH = File.expand_path("../../applications", __FILE__)
-CyberarmLauncher::CACHE_PATH = File.expand_path("../../data/.cache", __FILE__)
+CyberarmLauncher::CACHE_PATH = File.expand_path("../../data/cache", __FILE__)
